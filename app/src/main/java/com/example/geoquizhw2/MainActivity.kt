ab942dbd623e440de371ff6b49ee9c4986d26476
bgada <bga237@live.missouristate.edu>
2023-02-18 16:17:19 -0600

    Activity Life Cycle

--- app/src/main/java/com/example/geoquizhw2/MainActivity.kt
+++ app/src/main/java/com/example/geoquizhw2/MainActivity.kt
@@ -4,20 +4,31 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.geoquizhw2.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
-private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             val snackBar = Snackbar.make(
                 it,
                 "Correct",
@@ -25,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             )
             snackBar.show()
         }
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
             val snackBar = Snackbar.make(
                 it,
                 "Incorrect",
@@ -35,6 +46,34 @@ class MainActivity : AppCompatActivity() {
             snackBar.setTextColor(Color.BLACK)
             snackBar.setBackgroundTint(Color.RED)
             snackBar.show()
+
         }
+        binding.questionText.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex].textResId
+            //binding.questionText.setText(questionTextResId)
+            updateQuestion()
+
+        }
+        binding.previousButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex].textResId
+            //binding.questionText.setText(questionTextResId)
+            updateQuestion()
+
+        }
+
+        updateQuestion()
+        // call to the function
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionText.setText(questionTextResId)
+        // setting text to resource id
     }
 }
